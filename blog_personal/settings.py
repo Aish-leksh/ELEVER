4b9e017d0da0344b676e74f3c6b9342dbdab9641
aishujs10@gmail.com <aishujs10@gmail.comgit config --global user.email aishujs10@gmail.comgit config --global user.email aishujs10@gmail.com>
2026-07-14 22:57:46 +0530

    Prepare project for Render deployment

--- blog_personal/settings.py
+++ blog_personal/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-=6vt=fgn-tbi%g47%w-@4&lh%$g@qto2!hakz5r4m_70n!nq%(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aiswaryalekshmi.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com'
+]
 
 
 # Application definition
